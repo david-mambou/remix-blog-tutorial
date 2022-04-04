37c472f8d868817caa49ce2976e32fa13358456d
David Mambou <david.mambou@gmail.com>
2022-04-04 22:56:15 +0900

    tuto done

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,43 @@
+import { Link, json, useLoaderData } from "remix";
+import { getPosts } from "~/models/post.server";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: await getPosts()
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData();
+  console.log(posts);
+
+  return (
+    <main>
+      <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.slug}>
+            <Link
+              to={post.slug}
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
